d192b29848f41593a12157657dc889f42ee490af
Melony Showell <melony@Melonys-MacBook-Air.local>
2022-07-19 19:21:22 -0400

    Home and About

---
+++ src/pages/About.js
@@ -0,0 +1,13 @@
+import { useNavigate } from "react-router-dom"
+export function About() {
+  const navigate = useNavigate()
+  function goBack() {
+    navigate("/")
+  }
+  return (
+    <>
+    <h1>About</h1>
+    <button onClick={goBack}>back</button>
+    </>
+  )
+}
